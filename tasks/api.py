1fe1ef6e743cee2d86bb4443e10104b555149c6c
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-29 14:59:01 +0700

    [CHORE] Unbreak the try except for the list task api

--- tasks/api.py
+++ tasks/api.py
@@ -10,9 +10,12 @@ router = Router(auth=JWTAuth())
 
 @router.get("/", response={200: List[TaskSchema]})
 def list_tasks(request):
-    cycle = CycleService.get_active_cycle(request.auth)
-    tasks = ListServiceImpl.list_tasks(cycle.id)
-    return tasks
+    try:
+        cycle = CycleService.get_active_cycle(request.auth)
+        tasks = ListServiceImpl.list_tasks(cycle.id)
+        return tasks
+    except ValueError as e:
+        raise HttpError(400, str(e))
 
 
 @router.get("/sorted", response={200: SortedTaskSchema})
